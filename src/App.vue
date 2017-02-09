9ca966f38494fbd0ce2eb4252ce332a087d9571c
Martin <martinbutts@clear.net.nz>
2017-02-09 22:17:12 +1300

    add test routes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <h1>FINAPP</h1>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/registration">Registration</router-link>
+    <router-link to="/investments">Investments</router-link>
+    <router-link to="/contact">Contact</router-link>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+// import Home from './components/Home'
+// import About from './components/layout/About'
+// import Registration from './components/layout/Registration'
+// import Investments from './components/layout/Investments'
+// import Contact from './components/layout/Contact'
 export default {
   name: 'app'
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from 'components/Hello'
+import Home from 'components/Home'
+import About from 'components/layout/About'
+import Registration from 'components/layout/Registration'
+import Investments from 'components/layout/Investments'
+import Contact from 'components/layout/Contact'
 
 Vue.use(Router)
 
@@ -8,8 +12,29 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/registration',
+      name: 'Registration',
+      component: Registration
+    },
+    {
+      path: '/investments',
+      name: 'Investments',
+      component: Investments
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    },
+  ],
+  mode: 'history'
 })
