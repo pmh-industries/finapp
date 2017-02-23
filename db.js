4222a27537437672e9a117e49f623b9f2a6952f9
Martin <martinbutts@clear.net.nz>
2017-02-23 21:53:10 +1300

    add db.js

---
+++ db.js
@@ -0,0 +1,40 @@
+const clients = {
+  client1: {
+    name:'Veggie',
+    image: 'http://lorempixel.com/200/100/sports/1/',
+    account: 1234567,
+    investment: 999,
+    profit: 666,
+    status: 'active',
+    price:7.50
+  },
+  client2: {
+    name:'Meat',
+    image: 'http://lorempixel.com/200/100/sports/2/',
+    account: 2345678,
+    investment: 998,
+    profit: 667,
+    status: 'active',
+    price:6.50
+  },
+  client3: {
+    name:'Bruce',
+    image: 'http://lorempixel.com/200/100/sports/3/',
+    account: 3456789,
+    investment: 997,
+    profit: 668,
+    status: 'dead',
+    price:5.50
+  },
+  client4: {
+    name:'Eugene',
+    image: 'http://lorempixel.com/200/100/sports/4/',
+    account: 0123456,
+    investment: 996,
+    profit: 669,
+    status: 'active',
+    price:4.50
+  },
+  }
+
+export default burgers;
