602001bda6f8b1c7d21fb06f52d6dcf233a781ad
ikona23 <ikona23@gmail.com>
2017-02-23 18:57:29 +1300

    jquery

---
+++ src/components/dashboard/Dashboard.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div class="dash">
+  <iframe id="render">
+    <h1>Hello fro dashboard</h1>
+  </iframe>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style lang="css">
+</style>
+
