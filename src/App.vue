e1a76c18fdd55c3a1985665618d29657785e7bc2
ikona23 <ikona23@gmail.com>
2017-02-26 20:49:52 +1300

    dashboard finished

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@
               <router-link to="/contact">Contact</router-link>
             </li>
             <li>
-              <router-link to="/dashboard" class="btn btn-success">Personal account</router-link>
+              <router-link to="/dashboard" class="btn btn-success green">Personal account</router-link>
               </a></li>
           </ul>
         </div><!--/.nav-collapse -->
@@ -42,7 +42,87 @@
         <router-view></router-view>
       </div>
     </div>
-    //footer
+      <footer>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 col-sm-12 right-border">
+              <div class="footer-about">
+                <h2 class="footer-title">About Us</h2>
+                <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                  the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
+                  type and scrambled it to make a type specimen book. It has survived not only five centuries, but also
+                  the leap into electronic typesetting, remaining essentially unchanged. </p>
+              </div>
+              <div class="copyright hidden-sm hidden-xs">
+                <p>Copyrights &copy; 2017 <a href="http://nextbootstrap.com/" title="">pms solution</a>. All Right Reserved.</p>
+                <p>Developed By : <a href="#" title="">pms solution</a></p>
+              </div>
+            </div>
+            <div class="col-md-6 col-sm-12">
+              <div class="col-md-6 col-sm-6">
+                <div class="contact-info">
+                  <h2 class="footer-title">Contact Info</h2>
+                  <div class="single">
+                    <i class="fa fa-map-marker"></i>
+                    <p>99 Street Address,<br>
+                      Whatever, LKG 778569, Country</p>
+                  </div>
+                  <div class="single">
+                    <i class="fa fa-phone"></i>
+                    <p>+977-90123987280</p>
+                  </div>
+
+                  <div class="single">
+                    <i class="fa fa-envelope"></i>
+                    <p>info@example.com</p>
+                  </div>
+
+                  <div class="social-icon">
+                    <ul class="list-inline">
+                      <li><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
+                      <li><a href="#" title=""><i class="fa fa-twitter"></i></a></li>
+                      <li><a href="#" title=""><i class="fa fa-instagram"></i></a></li>
+                      <li><a href="#" title=""><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6 col-sm-6 left-border">
+                <div class="navigation">
+                  <h2 class="footer-title">Navigation</h2>
+                  <ul class="list-unstyled">
+                    <li><a href="#" title="">Home</a></li>
+                    <li><a href="#" title="">About Us</a></li>
+                    <li><a href="#" title="">Registration</a></li>
+                    <li><a href="#" title="">Terms & Conditions</a></li>
+                    <li><a href="#" title="">Privacy Policy</a></li>
+                    <li><a href="#" title="">Investment</a></li>
+                    <li><a href="#" title="">Sitemap</a></li>
+                    <li><a href="#" title="">Contact Us</a></li>
+                    <li><a href="#" title="">Personal Office</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="clearfix"></div>
+              <div class="copyright visible-sm visible-xs">
+                <p>Copyrights &copy; 2017 <a href="#" title="">pms solution</a>. All Right Reserved.</p>
+                <p>Developed By : <a href="#" title="">pms solution</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+    </template>
+
+    <script>
+      export default {
+      }
+    </script>
+
+    <style lang="css">
+    </style>
+
   </div>
 
 </template>
@@ -112,6 +192,9 @@
   footer .left-border {
     border-left: 1px solid rgba(255, 255, 255, 0.1);
   }
+  a.btn.btn-success{
+  color:white;
+}
 
   @media (max-width: 991px) {
     footer .copyright {
@@ -204,6 +287,8 @@
   footer .navigation li a:hover {
     color: #3ac341;
   }
+
+
 
   footer .copyright {
     padding-top: 40px;
