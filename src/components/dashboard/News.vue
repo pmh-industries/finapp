eb5280dbcb8d3d44f9395787f286bfb71ee72f1c
ikona23 <ikona23@gmail.com>
2017-04-15 23:58:25 +1200

    looking for solution

--- src/components/dashboard/News.vue
+++ src/components/dashboard/News.vue
@@ -1,23 +1,77 @@
 <template lang="html">
     <div id="root">
-
-        <div v-for="th in curr">
-        <button v-on:click="cash" class="btn-success" type="button">
-            {{th.title}}
+        <button v-on:click="complTskEur">
+to eur
         </button>
+        <ul>
+            <li v-for="th in curr" v-text="th.color" v-show="isVisible">
 
-        <p>{{th.num}}</p>
-        <p>{{th.num}}</p>
-        </div>
-        <button v-on:click="cash2" class="btn-success" type="button">{{title}}</button>
-        <h2 v-model= cash></h2>
+        </li>
+        </ul>
 
-            <div v-for="th in curr">
-                <button v-on:click="cash" class="btn-success" type="button">{{title}}</button>
-                <button v-on:click="cash2" class="btn-success" type="button">{{title}}</button>
-<h4 v-model="cash">{{dol}}</h4>
-            </div>
+            <article v-show="true">
+                this is the srticle
+            <button type='btn' @click="hideIt" class="close">x</button>
+            </article>
+        <hr>
+        <h4>all </h4>
+        <ul>
+            <li v-for="x in btns" v-text="x.description">
+
+            </li>
+
+        </ul>
+        <hr>
+        <h4>eur</h4>
+        <ul>
+            <li v-for="x in complTskEur" v-text="x.description">
+            <button>
+                change to eur
+
+                </button>
+
+            <h1>{{btns}}</h1>
+
+            </li>
+        </ul>
+
+<h1>heloo</h1>
         </div>
+
+        <!--<button v-on:click="add" class="btn-success" type="button">-->
+            <!--Chnge to dollars-->
+            <!--</button>-->
+        <!--<button v-on:click="mad" class="btn-success" type="button">-->
+            <!--Change to euros-->
+        <!--</button>-->
+        <!--<p>{{eur}}{{dol}}</p>-->
+<!--<br>-->
+        <!--<button v-on:click="mad" class="btn-success" type="button">-->
+            <!--New approach-->
+        <!--</button>-->
+        <!--<p>{{dol}}</p>-->
+
+        <!--<hr>-->
+
+        <!--<h1>hello</h1>-->
+        <!--<ul>-->
+
+        <!--<li v-for="x in butns" v-if="x.completed" v-text="x.description">-->
+        <!--<h1>Hello {{eur}}</h1>-->
+
+<!--</li>-->
+        <!--</ul>-->
+        <!--<hr>-->
+
+        <!--<div v-for="th in curr">-->
+        <!--<button v-on:click="cash" class="btn-success" type="button">-->
+           <!--change stuff {{th.title}}-->
+        <!--</button>-->
+        <!--<p>cash {{curr.usd.num}}  {{curr.usd.title}}</p>-->
+        <!--<p>{{th.num}}</p>-->
+            <!--<h1>v</h1>-->
+        <!--</div>-->
+        <!--</div>-->
 </template>
 
 <script>
@@ -27,11 +81,18 @@
         data() {
             el:"#root"
             return {
-                mess: "EUR / USD : 1",
+                isVisible: true,
+                eur: 33,
+                dol: "USD / EUR : 1",
+                btns: [
+                    {description: "USD / EUR : 1", usd: true},
+                    {description: "EUr / USD : 1", eur: false},
+                ],
                 curr: {
                     usd: {
-                        num:33,
-                        title:"dolla",
+                        completed: true,
+                        num: 33,
+                        title: "dolla",
                         color: "commision",
                         label: "Yellow package",
                         currency: "€",
@@ -42,8 +103,9 @@
                         val: 30 + "€"
                     },
                     eur: {
-                        num:2,
-                        title:"eur",
+                        completed: false,
+                        num: 2,
+                        title: "eur",
                         color: "commision",
                         label: "Yellow package",
                         currency: "€",
@@ -52,67 +114,11 @@
                         days: 365,
                         percentMonth: 12 + "%",
                         val: 30 + "€"
-                    },
-                },
-                curDol:'',
-                test:"test",
-                client: {
-                    name: 'Martin',
-                    surname: 'Butts',
-                    account: "1EG23456A7",
-                    amount: 0,
-                    investment: 999,
-                    profit: 636,
-                    price: 7.50,
-                    commission: [10, 12, 15, 16],
-                    amountOut: 0,
-                    amountIn: 190,
-                    amountBlocked:49,
-                    accountStatus: 22344,
-                    widthdraw: 200,
-                    amountInvested: 300,
-                    balance: 22144,
-                    days: [365, 720, "3 years"]
-                },
-                accountEur: {
-                    account: {
-                        amountOut: 0,
-                        amountIn: 190,
-                        amountBlocked:49,
-                    },
-                    yellow: {
-                        profit: 3338,
-                        commission: [10, 12, 15, 16],
-                        days: [365, 720, "3 years"],
-                        percentMonth: [12,54,53,22]
-                    },
-                    standard: {
-                        profit: 4438,
-                        commission: [10, 12, 15, 16],
-                        days: [365, 720, "3 years"],
-                        percentMonth: [12,54,53,22]
-                    },
-                    green: {
-                        profit: 4238,
-                        commission: [10, 12, 15, 16],
-                        days: [365, 720, "3 years"],
-                        percentMonth: [12,54,53,22]
-                    },
-                    exclusive: {
-                        profit: 4338,
-                        commission: [10, 12, 15, 16],
-                        days: [365, 720, "3 years"],
-                        percentMonth: [12,54,53,22]
-                    },
-                    blue: {
-                        profit: 5238,
-                        commission: [10, 12, 15, 16],
-                        days: [365, 720, "3 years"],
-                        percentMonth: [12,54,53,22]
                     }
-                }
+                },
             }
         },
+
         methods: {
             clicky: function () {
                 console.log("hello"+ this.currency)
@@ -120,18 +126,26 @@
 
             },
             add: function (event) {
-                console.log("hello form add")
+                console.log("hello form dollars")
+                this.eur
+            },
+            mad: function (event) {
+                console.log("hello form eur")
+                this.dol
             },
             cash: function (event){
                 console.log(this.curr.usd.num)
-                var res = this.curr.usd.num
-                return res
+                this.curr.usd.num = this.curr.eur.num
 
             },
             cash2: function (event){
                 console.log(this.curr.eur.num)
-                return this.curr.eur.num
+                this.curr.eur.num = this.curr.usd.num
+            },
+            hideIt(){
+                this.isVisible = !true
             }
+
 
         },
 
@@ -141,8 +155,10 @@
             },
             capitalizedMessage() {
                 return this.tasks.map(_.capitalize)
+            },
+            complTskEur(){
+                return this.btns.filter(btn => ! btn.usd)
             }
-
 
         }
     }
